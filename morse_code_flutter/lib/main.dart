7046dd9a3895a6219fcbca41e3cdd0f871696c1b
Jacob Anderson <jacob.anderson@learninga-z.com>
2019-05-16 18:23:59 -0400

    detects tap and long press

--- morse_code_flutter/lib/main.dart
+++ morse_code_flutter/lib/main.dart
@@ -1,52 +1,18 @@
-// Flutter code sample for material.IconButton.2
-
-// In this sample the icon button's background color is defined with an [Ink]
-// widget whose child is an [IconButton]. The icon button's filled background
-// is a light shade of blue, it's a filled circle, and it's as big as the
-// button is.
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: Text(_title)),
-        body: MyStatelessWidget(),
-      ),
-    );
-  }
-}
-
-/// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Ink(
-          decoration: ShapeDecoration(
-            color: Colors.red,
-            shape: CircleBorder(),
-          ),
-          child: IconButton(
-            icon: Icon(Icons.android),
-            color: Colors.white,
-            onPressed: () {
-              print("filled background");
-            },
-          ),
-        ),
-      ),
+    return new GestureDetector(
+      onTap: () => print('tapped!'),
+      onLongPress: () => print('long press!')
     );
   }
 }
