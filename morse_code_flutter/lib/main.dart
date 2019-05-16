5b93a0de8ace9abe452014eee5990277b609289d
Jacob Anderson <jacob.anderson@learninga-z.com>
2019-05-16 19:24:32 -0400

    sendSMS function created

--- morse_code_flutter/lib/main.dart
+++ morse_code_flutter/lib/main.dart
@@ -3,16 +3,28 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:sms/sms.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
+  SmsSender sender = new SmsSender();
+  SmsReceiver receiver = new SmsReceiver();
+
+  static String message = "This is a test message!";
+  static String jakesNumber = "6164506289";
+  List<String> recipents = [jakesNumber];
+
   @override
   Widget build(BuildContext context) {
     return new GestureDetector(
-      onTap: () => print('tapped!'),
+      onTap: () => print('tap!'),
       onLongPress: () => print('long press!')
     );
+  }
+
+  void _sendSMS(String message, List<String> recipients){
+    sender.sendSms(new SmsMessage(recipents[0], message));
   }
 }
