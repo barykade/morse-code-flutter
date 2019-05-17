b3ac25db003780452f3cfc6b3e2c54c39da5fb80
Jacob Anderson <jacob.anderson@learninga-z.com>
2019-05-17 12:55:46 -0400

    merge

--- morse_code_flutter/lib/chatscreen.dart
+++ morse_code_flutter/lib/chatscreen.dart
@@ -13,6 +13,12 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(chatroom.name)),
       body: _buildBody(context),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () {
+
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 
@@ -37,4 +43,55 @@ class ChatScreen extends StatelessWidget {
       children: snapshot.map((data) => buildMessageWidget(context, data)).toList(),
     );
   }
+
+  Widget buildMessageWidget(BuildContext context, DocumentSnapshot data) {
+    final message = Message.fromSnapshot(data);
+
+    return Padding(
+        key: ValueKey(message.text),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: ListTile(
+              title: Text(message.toString())         ),
+        ));
+  }
+
+
+}
+
+class Message {
+  final DocumentReference reference;
+  final DocumentReference chatroom;
+  final String text;
+
+  Message.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['text'] != null),
+        text = map['text'],
+        assert(map['chatroom'] != null),
+        chatroom = map['chatroom'];
+
+  Message.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "$text|" + chatroom.documentID;
+}
+
+class ChatRoom {
+  final String name;
+  final DocumentReference reference;
+
+  ChatRoom.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        name = map['name'];
+
+  ChatRoom.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "ChatRoom<$name>";
 }
