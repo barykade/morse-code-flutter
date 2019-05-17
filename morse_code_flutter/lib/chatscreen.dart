6c79bec198e38021fadbc5f01c1bb6547a4ec48c
Jacob Anderson <jacob.anderson@learninga-z.com>
2019-05-17 12:51:02 -0400

    add fab to chat screen

--- morse_code_flutter/lib/chatscreen.dart
+++ morse_code_flutter/lib/chatscreen.dart
@@ -12,6 +12,12 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(name)),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () {
+
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
